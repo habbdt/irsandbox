92db0beaa5e64dc791220a42211c8cbf7efd40e7
habbdt <habbdt@gmail.com>
2021-12-29 17:58:51 -0500

    nodejs app initial commit

---
+++ applications/nodejs-app/app.js
@@ -0,0 +1,6 @@
+var http = require('http');
+var os = require('os');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(`k8s pod hostname: ${os.hostname()}\n`);
+}).listen(8080);
